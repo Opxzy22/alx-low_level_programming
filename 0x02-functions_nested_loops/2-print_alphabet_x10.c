31b63c9a2835ee36c60152d3fbba55aa336e8314
Opeyemi <-Aopeyem84@gmail.com>
2022-10-19 04:31:12 -0700

    print

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,13 +4,13 @@
  * follow by a new line
  * return zero
  */
-void print_alphabetx10(void)
+void print_alphabet_x10(void)
 {
 	char i;
 	int j;
 
 	for (j = 0; j < 10; j++)
-{
+	{
 		for (i = 'a'; i <= 'z'; i++)
 			_putchar(i);
 	_putchar('\n');
