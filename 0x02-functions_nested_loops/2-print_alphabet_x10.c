89b4286bba706fef36bee1414e7e7d7ad7363c71
Opeyemi <-Aopeyem84@gmail.com>
2022-10-19 03:57:46 -0700

    print

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10- a funtion that print a alphabet x10
+ * follow by a new line
+ * return zero
+ */
+void print_alphabetx10(void)
+{
+	char i;
+	int j;
+
+	for (j = 0; j < 10; j++)
+{
+		for (i = 'a'; i <= 'z'; i++)
+			_putchar(i);
+	_putchar('\n');
+	}
+}
+
